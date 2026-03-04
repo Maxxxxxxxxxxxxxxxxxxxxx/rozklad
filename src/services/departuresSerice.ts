217bf85f733138a5a7e2b74d08c78e68de11d1ea
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2026-03-04 20:34:39 +0100

    feat: add fetchStopsData helper

--- src/services/departuresSerice.ts
+++ src/services/departuresSerice.ts
@@ -1,12 +1,29 @@
-import { API_BASE_URL } from "../constants";
-import type { DeparturesResponse } from "../types";
+import { API_BASE_URL, STOPS } from "../constants";
+import type { DeparturesResponse, StopData } from "../types";
 
 export const fetchDepartures = async (stopId: number) => {
-    const res = await fetch(`${API_BASE_URL}/departures?stopId=${stopId}`);
-    if (!res.ok) {
-        const errorText = await res.text();
-        throw new Error(`Failed to fetch departures for stopId ${stopId}: ${errorText}`);
-    }
+  const res = await fetch(`${API_BASE_URL}/departures?stopId=${stopId}`);
+  if (!res.ok) {
+    const errorText = await res.text();
+    throw new Error(
+      `Failed to fetch departures for stopId ${stopId}: ${errorText}`,
+    );
+  }
 
-    return await res.json() as DeparturesResponse;
- }
+  return (await res.json()) as DeparturesResponse;
+};
+
+export const fetchStopsData = async () => {
+  const promises = STOPS.map(async (stop) => {
+    const departuresResponse = await fetchDepartures(stop.stopId);
+    return {
+      ...stop,
+      departures: departuresResponse.departures,
+      lastUpdate: departuresResponse.lastUpdate,
+    } as StopData;
+  });
+
+  const res = await Promise.all(promises);
+
+  return res;
+};
