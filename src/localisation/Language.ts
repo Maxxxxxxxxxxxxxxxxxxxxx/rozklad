d018abf0efeee0bc08ed5af05305b724c79a068c
Max Schlamberger <m.schlamberger.674@studms.ug.edu.pl>
2026-09-04 01:29:47 +0200

    feat: add localization interfaces

---
+++ src/localisation/Language.ts
@@ -0,0 +1,14 @@
+export interface Language {
+  heading: string;
+  lastUpdate: string;
+  delay: string;
+  noDepartures: string;
+  vehicleNumber: string;
+  adminPanelConfirm: string;
+  adminPanelClear: string;
+  adminPanelSave: string;
+  adminPanelHeading: string;
+  adminPanelStopFormHeading: string;
+  adminPanelStopFormSelectStop: string;
+  adminPanelStopFormRemoveStop: string;
+}
---
+++ src/localisation/supported/polish.ts
@@ -0,0 +1,16 @@
+import type { Language } from "../Language";
+
+export const polish: Language = {
+  heading: "Odjazdy",
+  lastUpdate: "Ostatnia aktualizacja",
+  delay: "Opóźnienie",
+  noDepartures: "Brak odjazdów",
+  vehicleNumber: "Numer pojazdu",
+  adminPanelConfirm: "Potwierdź",
+  adminPanelClear: "Wyczyść",
+  adminPanelSave: "Zapisz",
+  adminPanelHeading: "Panel administratora",
+  adminPanelStopFormHeading: "Wybór przystanków",
+  adminPanelStopFormSelectStop: "Wybierz przystanek",
+  adminPanelStopFormRemoveStop: "Usuń przystanek",
+};
